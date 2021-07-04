d5a8a9b7603b9c05e146978358a88d8a023d848c
risitop <aziz.fouche@gmail.com>
2021-07-04 21:37:06 +0200

    Default dictionary: en

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -31,6 +31,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
+(setq ispell-dictionary "en")
 (setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
